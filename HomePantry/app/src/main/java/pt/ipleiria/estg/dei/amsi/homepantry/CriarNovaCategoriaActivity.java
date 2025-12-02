46bef9f91a5ead93f34ab18f0e2a74414ab539f4
EduMSantos2003 <2241535@my.ipleiria.pt>
2025-12-02 21:18:40 +0000

    02/12

---
+++ HomePantry/app/src/main/java/pt/ipleiria/estg/dei/amsi/homepantry/CriarNovaCategoriaActivity.java
@@ -0,0 +1,11 @@
+package pt.ipleiria.estg.dei.amsi.homepantry;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CriarNovaCategoriaActivity extends AppCompatActivity {
+
+    private ImageView imagemCategoria;
+    private Button btnCarregarFoto;
+    private EditText txtNomeCategoria;
+    private Button btnCriarCategoria;
+}
