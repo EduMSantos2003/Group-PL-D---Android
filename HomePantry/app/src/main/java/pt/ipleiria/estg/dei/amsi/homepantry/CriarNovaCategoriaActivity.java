c9129030fe7828706df21759df77da82218cd400
EduMSantos2003 <2241535@my.ipleiria.pt>
2025-12-06 17:42:25 +0000

    06/12
    Layouts das várias páginas

--- HomePantry/app/src/main/java/pt/ipleiria/estg/dei/amsi/homepantry/CriarNovaCategoriaActivity.java
+++ HomePantry/app/src/main/java/pt/ipleiria/estg/dei/amsi/homepantry/CriarNovaCategoriaActivity.java
@@ -1,5 +1,9 @@
 package pt.ipleiria.estg.dei.amsi.homepantry;
 
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CriarNovaCategoriaActivity extends AppCompatActivity {
